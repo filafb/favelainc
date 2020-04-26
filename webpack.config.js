fb4f9cba879930fa3b8957cb3ec344566fe43be9
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-04-26 13:36:59 -0400

    opt css output

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,22 @@
 const path = require("path")
 const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const TerserJSPlugin = require("terser-webpack-plugin")
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const purgecss = require("@fullhuman/postcss-purgecss")({
+  content: ["./client/**/*.js", "./client/**/*.jsx"],
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
 
 module.exports = (env, args) => {
-  console.log(path.resolve(__dirname, "./public/style"))
   return {
     mode: args.mode || "development",
     entry: ["@babel/polyfill", "./client/app"],
     output: {
       path: path.resolve(__dirname, "./public/"),
       filename: "./js/bundle.js"
+    },
+    optimization: {
+      minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
     },
     resolve: {
       extensions: [".js", ".jsx", ".scss", ".css"]
@@ -28,15 +36,36 @@ module.exports = (env, args) => {
           exclude: /node_modules/,
           use: [
             args.mode !== "production"
-              ? "style-loader"
+              ? { loader: "style-loader", options: { sourceMap: true } }
               : {
                   loader: MiniCssExtractPlugin.loader,
                   options: {
                     esModule: true
                   }
                 },
-            "css-loader", // translates CSS into CommonJS
-            "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            {
+              loader: "css-loader",
+              options: {
+                importLoaders: 1,
+                sourceMap: args.mode === "production"
+              }
+            }, // translates CSS into CommonJS
+            {
+              loader: "postcss-loader",
+              options: {
+                sourceMap: args.mode === "production",
+                ident: "postcss",
+                plugins: [
+                  require("tailwindcss"),
+                  require("autoprefixer"),
+                  ...(args.mode === "production" ? [purgecss] : [])
+                ]
+              }
+            },
+            {
+              loader: "sass-loader",
+              options: { sourceMap: args.mode === "production" }
+            } // compiles Sass to CSS, using Node Sass by default
           ]
         }
       ]
