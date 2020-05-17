d9559cfabbf3dd5451febd13bc2f12b4395f606a
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-05-16 20:47:13 -0400

    route to update user info

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 //any route plugged here are already mounted on `/api`
 router.use("/auth", require("./routes/auth"))
+router.use("/users", require("./routes/users"))
 
 //handleling 404
 router.use((req, res, next) => {
---
+++ server/api/routes/users/index.js
@@ -0,0 +1,40 @@
+const express = require("express")
+const router = express.Router()
+const { User } = require("../../../db")
+
+module.exports = router
+
+async function checkAllowToUpdate(req, res, next) {
+  try {
+    // check for user updating theirselves
+    const userToUpdate = await User.findOne({
+      where: { email: req.body.email }
+    })
+    res.locals.foundUser = userToUpdate
+    if (!userToUpdate) {
+      res.status(404).json({ error: "User not found" })
+    } else if (
+      req.user &&
+      req.user.id === userToUpdate.id &&
+      req.user.email === userToUpdate.email
+    ) {
+      next()
+      // check user asking for update is admin and user being updated is not admin
+    } else if (req.user && req.user.admin && !userToUpdate.admin) {
+      next()
+    } else {
+      res.status(401).json({ error: "Não autorizado" })
+    }
+  } catch (e) {
+    next(e)
+  }
+}
+
+router.put("/update", checkAllowToUpdate, async (req, res, next) => {
+  try {
+    await res.locals.foundUser.update(req.body)
+    res.sendStatus(204)
+  } catch (e) {
+    next(e)
+  }
+})
