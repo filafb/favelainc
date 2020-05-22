386c42094a82e01ad1512c92403c2ebd0b20fafb
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-05-21 22:28:08 -0400

    style for single user

--- client/components/Users/SingleUser.js
+++ client/components/Users/SingleUser.js
@@ -1,14 +1,19 @@
 import * as React from "react"
-import { useParams, useHistory } from "react-router-dom"
+import { useParams, useHistory, Link } from "react-router-dom"
 import { useSelector } from "react-redux"
 import useAuth from "../Hooks/useAuth"
+import AdminSvg from "../Svg/AdminSvg"
+import { SecondaryButton } from "../Partials/Buttons"
 
 const SingleUser = () => {
   const { id } = useParams()
   const users = useSelector(({ usersState: { users } }) => users)
   const history = useHistory()
   const [{ id: userLoggedId, admin }] = useAuth()
-  const [userToRender, setUserToRender] = React.useState({})
+  const [
+    { id: userId, firstName, lastName, email, admin: userAdmin },
+    setUserToRender
+  ] = React.useState({})
 
   const userToView = users.find(user => user.id === Number(id))
 
@@ -22,13 +27,25 @@ const SingleUser = () => {
     }
   }, [])
 
-  return userToRender.id ? (
-    <>
-      <div>firstName {userToRender.firstName}</div>
-      <div>lastName {userToRender.lastName}</div>
-      <div>email {userToRender.email}</div>
-      <div>admin {userToRender.admin}</div>
-    </>
+  return userId ? (
+    <main className="max-w-sm mx-auto">
+      <div className="mx-8 flex items-start justify-between">
+        <div>
+          <div className="flex items-center">
+            <p>{`${firstName} ${lastName}`}</p>
+            {userAdmin && <AdminSvg />}
+          </div>
+          <p>{email}</p>
+        </div>
+        {(!userAdmin || userId === userLoggedId) && (
+          <div>
+            <Link to={`/usuarios/${userId}/editar`}>
+              <SecondaryButton type="button" text="Editar" />
+            </Link>
+          </div>
+        )}
+      </div>
+    </main>
   ) : null
 }
 
