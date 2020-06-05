de32171bf4aa2d898e87f5297238e0311d1a3188
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-06-04 20:00:32 -0400

    basic list of partners

--- client/components/Partners/AllPartners.js
+++ client/components/Partners/AllPartners.js
@@ -1,7 +1,22 @@
 import * as React from "react"
+import { useSelector } from "react-redux"
 
 const AllPartners = () => {
-  return <div>Show List of partners</div>
+  const partners = useSelector(({ ngoPartnersState: { ngoList } }) => ngoList)
+
+  console.log(partners)
+
+  return (
+    <main className="max-w-sm mx-auto">
+      <ul className="mx-8">
+        {partners.map(({ id, name }) => (
+          <li key={id}>
+            <p>{name}</p>
+          </li>
+        ))}
+      </ul>
+    </main>
+  )
 }
 
 export default AllPartners
