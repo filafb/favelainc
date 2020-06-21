323acab7464fae8148a126a3a855e06eae993135
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-06-21 18:48:02 -0400

    added number of member in the query for families and filter based on user organization

--- server/api/routes/families/index.js
+++ server/api/routes/families/index.js
@@ -1,12 +1,34 @@
 const express = require("express")
 const router = express.Router()
-const { Family } = require("../../../db")
+const { Family, Resident } = require("../../../db")
+const { Sequelize } = require("sequelize")
 
 module.exports = router
 
 router.get("/", async (req, res, next) => {
   try {
-    const families = await Family.findAll()
+    const userOrg = await req.user.getNgoPartner()
+    const orgFilter = userOrg.master
+      ? {}
+      : {
+          ngoPartnerId: userOrg.id
+        }
+    const families = await Family.findAll({
+      where: { orgFilter },
+      attributes: {
+        include: [
+          [
+            Sequelize.fn("COUNT", Sequelize.col("residents.id")),
+            "familyMembers"
+          ]
+        ]
+      },
+      include: {
+        model: Resident,
+        attributes: []
+      },
+      group: ['"family.id"']
+    })
     res.json(families)
   } catch (error) {
     next(error)
